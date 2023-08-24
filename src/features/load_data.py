fixed data leakage

src/features/load_data.py
@@ -14,51 +14,76 @@ def load_data(as_3D=False):
     
     if exists(path_2d) and not as_3D:
         npz = np.load(path_2d)
-        return npz['X'], npz['y']
+        return npz['X_train'], npz['y_train'], npz['X_test'], npz['y_test']
     if exists(path_3d) and as_3D:
         npz = np.load(path_3d)
-        return npz['X'], npz['y']
+        return npz['X_train'], npz['y_train'], npz['X_test'], npz['y_test']
 
     df = pd.read_csv(abspath(join(dirname(dirname(dirname(__file__))), 'data/', 'processed/', 'extracted_stats.csv')))
 
-    df['date'] = pd.to_datetime(df['date']) # make sure logic can handle datetime for time series data 
     df.drop(df.loc[df['result'] == 'D'].index, inplace=True) # Drop ties first 
-    drop_cols = df.loc[:, :'referee'].columns # columns that will be dropped as features (strings, formats, etc.)
+    
+    # Splitting training and test set based on date of bout to prevent data leakage 
+    df['date'] = pd.to_datetime(df['date'])
+    df = df.loc[df['date'] > '2014-01-01'] 
+    test_df = df.loc[df['date'] > '2022-01-01'] 
+    train_df = df.drop(test_df.index)
 
+    # Dropping non-precomp features 
+    drop_cols = df.loc[:, :'referee'].columns 
     drop_cols = drop_cols.insert(0, df.columns[df.isna().mean() > 0.9].to_list()) # drop columns that contain more than % nans
-
-    post_comp_cols = df.loc[:, ~df.columns.str.contains('precomp_([a-zA-Z_]+)_vs_opp', regex=True)].columns.to_list() # All columns that do not have the precomp_ id or vs_opp id
+    post_comp_cols = df.loc[:, ~df.columns.str.contains('precomp_([a-zA-Z_]+)_vs_opp', regex=True)].columns.to_list() 
     drop_cols = drop_cols.insert(0, post_comp_cols)
 
+    # Dropping correlated features 
     features = df.drop(columns=drop_cols, axis=1)
     drop_features_cols = get_corr_features(features, thresh=0.95)
     drop_cols = drop_cols.insert(0, drop_features_cols)
 
     if as_3D:
-        multi_df = df.set_index(['fighter_url', df.groupby('fighter_url').cumcount(ascending=False)])
+        multi_df_train = train_df.set_index(['fighter_url', train_df.groupby('fighter_url').cumcount(ascending=False)])
         drop_cols = drop_cols.drop_duplicates().drop('fighter_url')
-        final_df = multi_df.drop(columns=drop_cols, axis=1)
+        final_df_train = multi_df_train.drop(columns=drop_cols, axis=1)
+
+        multi_df_test = test_df.set_index(['fighter_url', test_df.groupby('fighter_url').cumcount(ascending=False)])
+        final_df_test = multi_df_test.drop(columns=drop_cols, axis=1)
         
-        mask = multi_df.isna().mean(axis=1).groupby('fighter_url').mean() < 0.5           # Dropping Slices that are below some percent
+        mask_train = multi_df_train.isna().mean(axis=1).groupby('fighter_url').mean() < 0.5 
+        mask_test = multi_df_test.isna().mean(axis=1).groupby('fighter_url').mean() < 0.5          
         
-        X = final_df.loc[(mask[mask].index, slice(None)), :]
-        y = multi_df.loc[(mask[mask].index, slice(None)), ['result']]
+        X_train = final_df_train.loc[(mask_train[mask_train].index, slice(None)), :]
+        y_train = multi_df_train.loc[(mask_train[mask_train].index, slice(None)), ['result']]
 
-        X = X.to_xarray().to_array().to_numpy() # Feats x Fighter x Fights 
-        y = y.to_xarray().to_array().to_numpy() # 1 x Fighter x Fights
+        X_test = final_df_test.loc[(mask_test[mask_test].index, slice(None)), :]
+        y_test = multi_df_test.loc[(mask_test[mask_test].index, slice(None)), ['result']]
 
-        X = np.transpose(X, (1, 2, 0)) # Fighter x Fights x Feats
-        y = np.transpose(y, (1, 2, 0)) # Fighters x Fights x 1
+        X_train = X_train.to_xarray().to_array().to_numpy() # Feats x Fighter x Fights 
+        y_train = y_train.to_xarray().to_array().to_numpy() # 1 x Fighter x Fights
 
-        y[y == 'W'] = 1
-        y[y == 'L'] = 0
+        X_test = X_test.to_xarray().to_array().to_numpy() # Feats x Fighter x Fights 
+        y_test = y_test.to_xarray().to_array().to_numpy() # 1 x Fighter x Fights
 
-        X = X.astype(np.float32)
-        y = y.astype(np.float32)
+        X_train = np.transpose(X_train, (1, 2, 0)) # Fighter x Fights x Feats
+        y_train = np.transpose(y_train, (1, 2, 0)) # Fighters x Fights x 1
 
-        np.savez(path_3d, X=X, y=y)
+        X_test = np.transpose(X_test, (1, 2, 0)) # Fighter x Fights x Feats
+        y_test = np.transpose(y_test, (1, 2, 0)) # Fighters x Fights x 1
 
-        return X, y
+        y_train[y_train == 'W'] = 1
+        y_train[y_train == 'L'] = 0
+
+        y_test[y_test == 'W'] = 1
+        y_test[y_test == 'L'] = 0
+
+        X_train = X_train.astype(np.float32)
+        y_train = y_train.astype(np.float32)
+
+        X_test = X_test.astype(np.float32)
+        y_test = y_test.astype(np.float32)
+
+        np.savez(path_3d, X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
+
+        return X_train, y_train, X_test, y_test
 
     else:
         final_df = df.drop(columns=drop_cols, axis=1)
@@ -74,7 +99,7 @@ def load_data(as_3D=False):
         X = X.to_numpy(dtype=np.float32)
         y = np.reshape(y.to_numpy(dtype=np.float32), (-1,1))
 
-        np.savez(path_3d, X=X, y=y)
+        np.savez(path_2d, X=X, y=y)
 
         return X, y
             

src/models/Pytorch_Model.py
@@ -29,7 +29,7 @@ class MMADataset(Dataset):
         super(MMADataset, self).__init__()
         
         # Dataloading
-        self.X, self.y = load_data(as_numpy=True)
+        self.X, self.y = load_data(as_3D=False)
         self.X = np.nan_to_num(self.X)
         self.X, self.y = torch.from_numpy(self.X), torch.from_numpy(self.y)
         
@@ -77,7 +77,7 @@ sc.fit(train[:][0])
 # Hyperparameters
 input_size = dataset[:][0].shape[1]
 hidden1_size = 500
-hidden2_size = 100
+hidden2_size = 200
 hidden3_size = 25
 output_size = 1
 batch_size = 50
@@ -137,7 +137,7 @@ model = MMANet(input_size=input_size,
 # Loss and optimizer
 criterion = nn.BCEWithLogitsLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=5)
+scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.999, patience=5)
 
 # Tensorboard
 writer.add_graph(model, iter(train_loader).__next__()[0].to(device))

src/models/Pytorch_model2.py
@@ -1,14 +1,27 @@
+import copy
 import datetime
 import sys
+import time
 from typing import Any
 
 import matplotlib.pyplot as plt
+import mlflow
 import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim.lr_scheduler as lr_scheduler
-from torch.utils.data import DataLoader, Dataset, random_split
+from mlflow.tracking import MlflowClient
+from ray import air, tune
+from ray.air import session
+from ray.air.checkpoint import Checkpoint
+from ray.air.integrations.mlflow import MLflowLoggerCallback
+from ray.tune.schedulers import ASHAScheduler
+from ray.tune.search.bayesopt import BayesOptSearch
+from ray.tune.search.concurrency_limiter import ConcurrencyLimiter
+from ray.tune.search.optuna import OptunaSearch
+from sklearn.model_selection import KFold
+from torch.utils.data import DataLoader, Dataset, Subset, random_split
 from torch.utils.tensorboard.writer import SummaryWriter
 from torchvision.transforms import Compose
 
@@ -23,11 +36,14 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Dataset Class
 class MMADataset3D(Dataset):
-    def __init__(self, transform=None, target_transform=None):
+    def __init__(self, transform=None, target_transform=None, train=True):
         super(MMADataset3D, self).__init__()
         
         # Dataloading
-        self.X, self.y = load_data(as_3D=True)
+        if train: 
+            self.X, self.y, _, _ = load_data(as_3D=True)
+        else:
+            _, _, self.X, self.y = load_data(as_3D=True)
         
         self.transform = transform
         self.target_transform = target_transform
@@ -58,9 +74,6 @@ class ToTensor(object):
         X, y = sample
         return torch.from_numpy(X), torch.from_numpy(y)
 
-dataset = MMADataset3D(transform=Compose([CutFiller(), 
-                                          ToTensor()]))
-
 # Scaling function for standardizing columns
 class StandardScaler:
     def __call__(self, X):
@@ -70,27 +83,6 @@ class StandardScaler:
     def fit(self, X):
         self.mean = np.nanmean(X, 0, keepdims=True)
         self.std = np.nanstd(X, 0, keepdims=True)
-
-# Split datasets 
-train, val, test = random_split(dataset=dataset, 
-                           lengths=[0.8, 0.1, 0.1])
-
-# Standardize dataset 
-sc = StandardScaler()
-mat = train[0][0]
-for i in range(1, len(train)):
-    mat = np.vstack((mat, train[i][0]))
-sc.fit(mat)
-
-# Hyperparameters
-input_size = dataset[0][0].shape[1]
-hidden1_size = 500
-hidden2_size = 100
-hidden3_size = 25
-output_size = 1
-batch_size = 5
-lr = 1e-3
-epochs = 400
 
 # Colating function for dataloader to handle variable sequence lengths
 class PadSequence():
@@ -102,75 +94,151 @@ class PadSequence():
         labels = [x[1] for x in sorted_batch]
         labels_padded = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True)
         return sequences_padded, lengths, labels_padded
-
-# Dataloader 
-train_loader = DataLoader(dataset=train, 
-                          batch_size=batch_size,
-                          shuffle=True,
-                          collate_fn=PadSequence())
-
-val_loader = DataLoader(dataset=val, 
-                          batch_size=batch_size,
-                          shuffle=False,
-                          collate_fn=PadSequence())
-
-test_loader = DataLoader(dataset=test,
-                         batch_size=batch_size,
-                         shuffle=False,
-                         collate_fn=PadSequence())
-
-loaders = {'train': train_loader,
-           'val': val_loader,
-           'test': test_loader}
-
-datasets = {'train': train,
-            'val': val,
-            'test': test}
-
+    
 # Model Architecture 
 class MMA_RNN(nn.Module):
-    def __init__(self, input_size, hidden1_size, output_size):
+    def __init__(self, input_size, hidden_size, num_layers, dropout, output_size):
         super().__init__()
 
-        self.rnn = nn.RNN(input_size, 
-                          hidden1_size, 
-                          num_layers=1)
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+
+        self.rnn = nn.LSTM(input_size=input_size, 
+                          hidden_size=hidden_size, 
+                          num_layers=num_layers)
         
-        self.l1 = nn.Linear(hidden1_size,
+        self.drop = nn.Dropout(p=dropout)
+        
+        self.l1 = nn.Linear(hidden_size,
                             output_size)
 
     def forward(self, batch):
         X, lengths, _ = batch
         X_packed = torch.nn.utils.rnn.pack_padded_sequence(X, lengths, batch_first=True)
 
-        out, hidden = self.rnn(X_packed)
-        out, lens_unpacked = nn.utils.rnn.pad_packed_sequence(out, batch_first=True)
+        h0 = torch.zeros(self.num_layers, X.size(0), self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, X.size(0), self.hidden_size).to(device)
 
+        out, hidden = self.rnn(X_packed, (h0, c0))
+        out, lens_unpacked = nn.utils.rnn.pad_packed_sequence(out, batch_first=True)
+        out = self.drop(out)
         out = self.l1(out)
         return out
 
-# Model
-model = MMA_RNN(input_size, hidden1_size, output_size).to(device)
+# Training function
+def train_model(config, train, val):
 
-# Loss and optimizer 
-criterion = nn.BCEWithLogitsLoss(reduction='none')
-optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    # Model Initialization 
+    model = MMA_RNN(input_size=input_size, 
+                        hidden_size=config['hidden_size'], 
+                        num_layers=config['num_layers'], 
+                        dropout=config['dropout'],
+                        output_size=output_size).to(device)
 
-for epoch in range(epochs):
-        
-    for phase in ['train', 'val']:
-        
-        # Set model to proper mode
-        if phase == 'train':
-            model.train()
-        else:
-            model.eval()
+    # Standardize dataset 
+    sc = StandardScaler()
+    mat = train[0][0]
+    for i in range(1, len(train)):
+        mat = np.vstack((mat, train[i][0]))
+    sc.fit(mat)
 
+    # Dataloaders 
+    train_loader = DataLoader(dataset=train, 
+                            batch_size=config['batch_size'],
+                            shuffle=True,
+                            collate_fn=PadSequence())
+
+    val_loader = DataLoader(dataset=val, 
+                            batch_size=config['batch_size'],
+                            shuffle=False,
+                            collate_fn=PadSequence())
+
+    loaders = {'train': train_loader,
+                'val': val_loader}
+
+    # Loss and Optimizer
+    criterion = nn.BCEWithLogitsLoss(reduction='none')
+    optimizer = torch.optim.SGD(model.parameters(), lr=config['lr'])
+
+    # Training Loop
+    while True:
+      
+        for phase in ['train', 'val']:
+            
+            # Set model to proper mode
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+            running_total = 0
+
+            for i, batch in enumerate(loaders[phase]):
+
+                # Unpack batch
+                X, l, y = [b.to(device) for b in batch]
+                
+                # Scale X, l requires to be on cpu
+                X = torch.nan_to_num(sc(X))
+                l = l.to('cpu')
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    
+                    # Forward 
+                    outputs = model([X,l,y])
+
+                    # Loss, Raw per individual sequence 
+                    loss = criterion(outputs, y)
+
+                    # Make masking array based on l
+                    mask = np.full((len(l), l[0], 1), False)
+                    for i in range(0,len(l)):
+                        mask[i,0:l[i],0] = True
+                    mask = torch.tensor(mask)
+                    
+                    # Mean Loss Computed
+                    loss[~mask] = 0
+                    loss = loss[mask].sum() / mask.sum()
+
+                    # Backwards
+                    if phase == 'train':
+                        optimizer.zero_grad()
+                        loss.backward()
+                        optimizer.step()
+
+                # Predicting values 
+                pred = torch.sigmoid(outputs)
+                results = (pred > 0.5).float()
+
+                running_loss += loss.item() * mask.sum()
+                running_corrects += (results == y)[mask].sum().item()
+                running_total += mask.sum()
+
+            # Compute Epoch wide loss and accuracy
+            epoch_loss = running_loss / running_total
+            epoch_acc = running_corrects / running_total
+            # If phase is validation, add to trial length list 
+            if phase == 'val':
+                # Save Epoch loss and wts for checkpoint
+                checkpoint_data = {'wts': model.state_dict()}
+                checkpoint = Checkpoint.from_dict(checkpoint_data)
+                session.report({'loss': epoch_loss.item(),
+                                'acc': epoch_acc.item()}, checkpoint=checkpoint)
+
+def test_model(model, test_loader, sc):
+
+    criterion = nn.BCEWithLogitsLoss(reduction='none')
+
+    with torch.no_grad():
         running_loss = 0.0
         running_corrects = 0
         running_total = 0
 
-        for i, batch in enumerate(loaders[phase]):
+        model.eval()
+
+        for batch in test_loader:
 
             # Unpack batch
             X, l, y = [b.to(device) for b in batch]
@@ -179,38 +247,134 @@ for epoch in range(epochs):
             X = torch.nan_to_num(sc(X))
             l = l.to('cpu')
 
-            with torch.set_grad_enabled(phase == 'train'):
-                
-                # Forward 
-                outputs = model([X,l,y])
+            # Forward 
+            outputs = model([X,l,y])
 
-                # Loss
-                loss = criterion(outputs, y)
+            # Loss, Raw per individual sequence 
+            loss = criterion(outputs, y)
 
-                # Make masking array based on l
-                mask = np.full((len(l), l[0], 1), False)
-                for i in range(0,len(l)):
-                    mask[i,0:l[i],0] = True
-                mask = torch.tensor(mask)
-                
-                loss[~mask] = 0
-                loss = loss[mask].sum() / mask.sum()
-
-                if phase == 'train':
-                    # Backwards
-                    optimizer.zero_grad()
-                    loss.backward()
-                    optimizer.step()
-
-            threshold = torch.tensor([0.5]).to(device)
+            # Make masking array based on l
+            mask = np.full((len(l), l[0], 1), False)
+            for i in range(0,len(l)):
+                mask[i,0:l[i],0] = True
+            mask = torch.tensor(mask)
+            
+            # Mean Loss Computed
+            loss[~mask] = 0
+            loss = loss[mask].sum() / mask.sum()
+            
+            # Convert to sigmoid layer, threshold at 0.5 and convert to 0,1
             pred = torch.sigmoid(outputs)
             results = (pred > 0.5).float()
 
             running_loss += loss.item() * mask.sum()
-            running_corrects += (results == y)[mask].sum().item()
             running_total += mask.sum()
+            running_corrects += (results == y)[mask].sum().item()
 
-        epoch_loss = running_loss / running_total
-        epoch_acc = running_corrects / running_total
+        loss = running_loss / running_total
+        acc = running_corrects / running_total
+        print(f'Accuracy = {acc}, Loss = {loss}')
 
-        print(f'Current Phase: {phase}, Epoch: {epoch+1}, Current Loss: {epoch_loss:.6f}, Current Acc: {epoch_acc:.6f}')
+if __name__=='__main__':
+
+    # Mlflow setup
+    client = MlflowClient()
+    mlflow_tracking_uri = mlflow.get_tracking_uri()
+    mlflow.set_tracking_uri(mlflow_tracking_uri)
+    exp_name = 'RNN-'+ datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    experiment_id = client.create_experiment(name=exp_name)
+
+    # Dataset loading
+    train = MMADataset3D(train=True,
+                        transform=Compose([CutFiller(), 
+                                            ToTensor()]))
+    
+    test = MMADataset3D(train=False,
+                        transform=Compose([CutFiller(), 
+                                            ToTensor()]))
+
+    # Split datasets 
+    val, test = random_split(dataset=test, 
+                            lengths=[0.9, 0.1])
+
+    # Search space for hyperparameters and ML Flow 
+    search_space = {'mlflow_experiment_id': experiment_id,
+                    'lr': tune.loguniform(1e-3, 1e-2),
+                    'hidden_size': tune.qrandint(600,800,1),
+                    'batch_size': tune.choice([2,4,8]),
+                    'num_layers': tune.choice([1]),
+                    'dropout': tune.uniform(0.2,0.5)
+                    }
+    # search_space = {'mlflow_experiment_id': experiment_id,
+    #                 'lr': 5e-2,
+    #                 'hidden_size': 55,
+    #                 'batch_size': 2,
+    #                 'num_layers': tune.choice([1])
+    #                 }
+
+    # Hyperparameters
+    input_size = train[0][0].shape[1]
+    output_size = 1
+
+    # Hyperparameter search 
+    algo = OptunaSearch()
+    algo = ConcurrencyLimiter(algo, max_concurrent=4)
+
+    tuner = tune.Tuner(
+        tune.with_resources(
+            tune.with_parameters(trainable=train_model, train=train, val=val),
+            resources={'cpu': 4, 'gpu': 0.25}
+        ),
+        tune_config=tune.TuneConfig(
+            num_samples=50,
+            scheduler=ASHAScheduler(
+                max_t=200,
+                grace_period=3
+            ),
+            metric='loss',
+            mode='min',
+            search_alg=algo,
+        ),
+        run_config=air.RunConfig(
+            stop={"training_iteration": 200},
+            callbacks=[MLflowLoggerCallback(
+                experiment_name=exp_name)],
+        ),
+        param_space=search_space
+    )
+
+    results = tuner.fit()
+    best_model = results.get_best_result(scope='all')
+    best_config = best_model.config
+    print('Best config is: ', best_model.config)
+    
+    best_checkpoint = best_model.get_best_checkpoint('loss', 'min')
+    checkpoint_data = best_checkpoint.to_dict()
+
+    # Init best config model with appropriate weights 
+    config = best_model.config
+    best_model = MMA_RNN(input_size=input_size, 
+                            hidden_size=best_config['hidden_size'], 
+                            num_layers=best_config['num_layers'], 
+                            dropout=best_config['dropout'],
+                            output_size=output_size).to(device)
+    best_model.load_state_dict(checkpoint_data['wts'])
+
+    # Standardize dataset 
+    sc = StandardScaler()
+    mat = train[0][0]
+    for i in range(1, len(train)):
+        mat = np.vstack((mat, train[i][0]))
+    sc.fit(mat)
+
+    test_loader = DataLoader(dataset=test,
+                    batch_size=best_config['batch_size'],
+                    shuffle=False,
+                    collate_fn=PadSequence())
+
+    test_model(best_model, test_loader, sc)
+
+# Tensorboard
+# writer.add_graph(model, iter(train_loader).__next__()[0][0].to(device))
+# writer.close()
+